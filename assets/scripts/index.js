0be7bbf828cd5ac1e2a14ddfb8e946f5d0c713cb
ryan <ryanwkoch@gmail.com>
2017-06-29 14:27:02 -0400

    buttons now work
    added functionality to sign-in/sign-up/sign-out buttons. These
    buttons are now communicating with the api and can create, store, and
    remove user information
    
    these two steps are now complete:
    Write jQuery code to handle browser interaction.
    Start communicating with the back-end using curl. Use this to begin
    writing your AJAX code.
    
    next steps:
    Communicate with a back-end to store
    the state of my game.
    
    brainstorm:
    use patch requests to keep the server informed of every time anything
    happens to the game (when an x or o is placed on the board)
    Each click should trigger something that sends a patch request
    check the Api-doc
    investigate (data.games.length for total games)
    make it look high quality

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,7 @@
 'use strict'
+// a function given to me to deal with inputs on forms
+const getFormFields = require('../../lib/get-form-fields')
+const api = require('./api')
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
@@ -7,15 +10,31 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-//
+// links to events.js file and adds handlers
 const events = require('./events')
 
 require('./example')
 
 $(() => {
   events.addHandlers()
+})
+
+// checks if doc is ready and uses jQuery to
+// handle requests to the api when buttons are clicked
+$(() => {
+  $('#sign-up-form').on('submit', function (e) {
+    const data = getFormFields(this)
+    e.preventDefault()
+    api.signUpRequest(data)
+  })
+  $('#sign-in-form').on('submit', function (e) {
+    const data = getFormFields(this)
+    e.preventDefault()
+    api.signInRequest(data)
+  })
+  $('#sign-out').on('click', function (e) {
+    // const data = getFormFields(this)
+    e.preventDefault()
+    api.signOut()
+  })
 })
